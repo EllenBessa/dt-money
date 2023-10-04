4f92239e97a7a63846b507636379e41fe48e8a01
EllenBessa <ellenbessa96@gmail.com>
2023-10-04 10:51:43 -0300

    feat: :lipstick: Estrutura visual - Botões de entrada e saída

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -79,3 +79,36 @@ export const CloseButton = styled(Dialog.Close)`
   top: 1.5rem;
   right: 1.5rem;
 `;
+
+export const TransactionType = styled.div`
+  margin-top: 0.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+`;
+
+interface TransactionTypeButtonProps {
+  variant: "income" | "outcome";
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  padding: 1rem;
+  background: ${(props) => props.theme["gray-700"]};
+  color: ${(props) => props.theme["gray-300"]};
+  border: 0;
+  border-radius: 6px;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  svg {
+    color: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-300"]
+        : props.theme["red-300"]};
+  }
+`;
